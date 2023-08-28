ac78464f46c7bc265deff997a69200eb255628d0
LuneFleurs <sooda1014@gmail.com>
2023-08-28 22:08:01 +0900

    csp 삭제

--- pages/_document.js
+++ pages/_document.js
@@ -19,10 +19,6 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content="default-src 'self'; style-src 'self' 'unsafe-inline' https://giscus.app; font-src 'self' https://fonts.gstatic.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://giscus.app;"
-          />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
