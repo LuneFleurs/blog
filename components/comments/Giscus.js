a44d8c30ee6add7c235bb9284c29615fff50c5be
LuneFleurs <sooda1014@gmail.com>
2023-08-29 17:33:03 +0900

    테마 버그 수정 제발

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 import { useTheme } from 'next-themes'
 import siteMetadata from '@/data/siteMetadata'
 
 const Giscus = () => {
   const { theme, resolvedTheme } = useTheme()
-
-  const commentsTheme =
+  const [commentsTheme, setCommentsTheme] = useState(
     theme === 'dark' || resolvedTheme === 'dark'
       ? siteMetadata.comment.giscusConfig.darkThemeURL
       : siteMetadata.comment.giscusConfig.lightThemeURL
+  )
 
   const COMMENTS_ID = 'comments-container'
 
-  const LoadComments = useCallback(() => {
+  const LoadComments = useCallback((themeToUse) => {
     const {
       repo,
       repositoryId,
@@ -36,46 +36,37 @@ const Giscus = () => {
     script.setAttribute('data-emit-metadata', metadata)
     script.setAttribute('data-input-position', inputPosition)
     script.setAttribute('data-lang', lang)
-    script.setAttribute('data-theme', commentsTheme)
+    script.setAttribute('data-theme', themeToUse)
     script.setAttribute('crossorigin', 'anonymous')
     script.async = true
 
     const comments = document.getElementById(COMMENTS_ID)
     if (comments) comments.appendChild(script)
-
-    return () => {
-      const comments = document.getElementById(COMMENTS_ID)
-      if (comments) comments.innerHTML = ''
-    }
-  }, [commentsTheme])
+  }, [])
 
   const handleThemeChange = useCallback(
     (event) => {
       const newTheme = event.detail.newTheme
-      // 일부 지연 후 댓글을 다시 로드
-      setTimeout(() => {
-        LoadComments()
-      }, 100)
+      const newCommentsTheme =
+        newTheme === 'dark'
+          ? siteMetadata.comment.giscusConfig.darkThemeURL
+          : siteMetadata.comment.giscusConfig.lightThemeURL
+
+      // Update the comments theme state
+      setCommentsTheme(newCommentsTheme)
+
+      // Reload the comments with the new theme
+      LoadComments(newCommentsTheme)
     },
     [LoadComments]
   )
 
   useEffect(() => {
-    const giscusCSSLink = document.getElementById('giscus-theme-css')
-    const newCSSHref = commentsTheme
+    // Your existing logic for CSS link updates
+    // ...
 
-    if (giscusCSSLink) {
-      giscusCSSLink.href = newCSSHref
-    } else {
-      const link = document.createElement('link')
-      link.id = 'giscus-theme-css'
-      link.rel = 'stylesheet'
-      link.href = newCSSHref
-      document.head.appendChild(link)
-    }
-
-    const iframe = document.querySelector('iframe.giscus-frame')
-    if (!iframe) LoadComments()
+    // Initialize or reload comments
+    LoadComments(commentsTheme)
 
     // Listen for the custom event for theme change
     window.addEventListener('themeChanged', handleThemeChange)
@@ -85,7 +76,7 @@ const Giscus = () => {
       // Remove the event listener when the component is unmounted
       window.removeEventListener('themeChanged', handleThemeChange)
     }
-  }, [theme, resolvedTheme, LoadComments, handleThemeChange])
+  }, [theme, resolvedTheme, LoadComments, handleThemeChange, commentsTheme])
 
   return (
     <div className="w-full pb-6 pt-6 text-center text-gray-700 dark:text-gray-300">
